ed20698933945ef033b1724d7954021939a2697a
epchao <46041923+epchao@users.noreply.github.com>
2019-12-25 21:56:52 -0800

    Update scrollAnim.js

--- scripts/scrollAnim.js
+++ scripts/scrollAnim.js
@@ -44,25 +44,6 @@ $(window).on("load", function() {
       }
     });
 
-    $(".howto-steps").each(function() {
-      //Declare a variable that shows the offset of the top layer and the outerHeight of an element
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-      //checks to see if the bottom of the object > the bottom of the window viewer
-      if (objectBottom < windowBottom) {
-        //if the opacity of this element is set to 0 then it will fade to 500 and view the element.
-        if ($(this).css("opacity") == 0) {
-          $(this).fadeTo(500, 1);
-        }
-      else {
-        //if opacity of element is set to 1 then it will fade to 500 and view the element. Reload page to get fade animation again
-        if ($(this).css("opacity") == 1){
-          $(this).fadeTo(500, 1);
-        }  
-      }
-      }
-    });
-
     // scroll through page
   }).scroll();
 });
